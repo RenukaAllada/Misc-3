529d18b0153aa91ee15a9a587975c122543eb508
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-04-19 13:54:16 -0500

    Completed 2 exercises

--- Sample.java
+++ Sample.java
@@ -44,4 +44,58 @@ class Solution {
     }
 }
 /********************PROBLEM-2*****************/
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    //TC:0(N)
+    //SC:0(1)
+    class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            if(head==null || k<=1){
+                return head;
+            }
+
+            ListNode dummy=new ListNode(-1);
+            dummy.next=head;
+
+            int count=0;
+            ListNode begin=dummy;
+            while(head!=null){
+                count++;
+                if(count%k==0){
+                    begin=reverse(begin,head.next);
+                    head=begin;
+                }
+                head=head.next;
+            }
+
+            return dummy.next;
+        }
+
+        private ListNode reverse(ListNode begin,ListNode end){
+            ListNode first=begin.next;
+            ListNode curr=begin.next;
+            ListNode prev=null;
+            ListNode fast=curr.next;
+            while(fast!=end){
+                curr.next=prev;
+                prev=curr;
+                curr=fast;
+                fast=fast.next;
+            }
+            curr.next=prev;
+            first.next=end;
+            begin.next=curr;
+
+            return first;
+        }
+    }
 }
